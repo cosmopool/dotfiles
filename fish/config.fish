5630747e633640c981a23d7766b615c99b09aee6
Kaio Delphino <dash.zet@gmail.com>
2025-02-25 13:34:35 -0300

    feat: setup fvm and android path

--- fish/config.fish
+++ fish/config.fish
@@ -14,16 +14,17 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 # set -gx XDG_RUNTIME_DIR /run/user/$(id -u)
 
-set ANDROID_SDK_ROOT "/$HOME/.local/share/android-sdk"
+# set -gx ANDROID_SDK_ROOT "$HOME/Library/Android/Sdk"
+set -gx ANDROID_SDK_ROOT "$HOME/.local/share/android-sdk"
 set A_SDK_TOOLS $ANDROID_SDK_ROOT"/cmdline-tools/latest/bin"
-# set A_SDK_TOOLS_B $ANDROID_SDK_ROOT"/tools"
-# set A_SDK_TOOLS_C $A_SDK_TOOLS_B"/tools/bin"
 set A_SDK_PLATFORM_T $ANDROID_SDK_ROOT"/platform-tools"
 set A_SDK_EMULATOR $ANDROID_SDK_ROOT"/emulator"
+# set -gx ANDROID_AVD_HOME "$HOME/.android/avd/"
+set FLUTTER_FVM_PATH "$HOME/fvm/default/bin"
 set -gx JAVA_OPTS "-XX:+IgnoreUnrecognizedVMOptions"
 
-set -gx PATH $PATH $ANDROID_SDK_ROOT $A_SDK_TOOLS $A_SDK_EMULATOR $A_SDK_PLATFORM_T "$HOME/.local/bin" "$HOME/bin" "$HOME/.pub-cache/bin" "$HOME/bin" "$GOPATH/bin" "$HOME/.local/share/flatpak/exports/share" "$HOME/Git/v"
-# set -gx PATH $PATH "$HOME/.local/bin" "$HOME/bin" $A_SDK_TOOLS $A_SDK_TOOLS_B $A_SDK_TOOLS_C $A_SDK_CMD $A_SDK_PLATFORM_T $A_SDK_EMULATOR "$HOME/.pub-cache/bin" "$HOME/bin" "$GOPATH/bin" "$HOME/.local/share/flatpak/exports/share" "$HOME/Git/v"
+set -gx PATH $PATH $FLUTTER_FVM_PATH $ANDROID_SDK_ROOT $A_SDK_TOOLS $A_SDK_EMULATOR $A_SDK_PLATFORM_T "$HOME/.local/bin" "$HOME/bin" "$HOME/.pub-cache/bin" "$HOME/bin" "$GOPATH/bin" "$HOME/.local/share/flatpak/exports/share" "$HOME/Git/v"
+# set -gx PATH $PATH $FLUTTER_FVM_PATH "$HOME/.local/bin" "$HOME/bin" "$GOPATH/bin" "$HOME/.local/share/flatpak/exports/share"
 
 alias mpv-yt "mpv --ytdl-format=bestvideo[vcodec=vp9]+bestaudio/best"
 alias yt "mpv --hwdec=auto"
